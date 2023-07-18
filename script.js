6603bb4b5fbb0eb388036a37e4818f37bc3f91b4
Ashwinirani1433 <130225137+Ashwinirani1433@users.noreply.github.com>
2023-07-18 11:20:23 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 function threeSum(arr, target) {
 // write your code here
-	let sum = 0;
-	for(let i = 0; i < arr.length; i++){
-		sum = arr[i]+arr[i+1]+arr[i+2];
-		if(sum == target + 1){
-			return sum;
-		}
-	}
-  return sum;
-}
+	arr.sort((a, b) => a - b);
+  
+  // Initialize variables
+  let closestSum = arr[0] + arr[1] + arr[2];
+  
+  // Iterate through the array
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+    
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      
+      // Check if the current sum is closer to the target
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+      
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        // Found an exact match, so return the sum
+        return sum;
+      }
+    }
+  }
 
 module.exports = threeSum;
